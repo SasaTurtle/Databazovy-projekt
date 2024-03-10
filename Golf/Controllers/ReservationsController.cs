65a4b9be6963d8edd5f89936a43ffc2cbcedf6f9
SasaTurtle <sascha.kominek@gmail.com>
2024-03-10 11:39:27 +0100

    Databazovy projekt s nazvem Golf

---
+++ Golf/Controllers/ReservationsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Golf.Models;
+
+namespace Golf.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly GolfContext _context;
+
+        public ReservationController(GolfContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Reservation
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Reservation>>> GetReservation()
+        {
+          if (_context.Reservation == null)
+          {
+              return NotFound();
+          }
+            return Ok(await _context.Reservation.ToListAsync());
+        }
+
+        // GET: api/Reservation/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Reservation>> GetReservation(int id)
+        {
+          if (_context.Reservation == null)
+          {
+              return NotFound();
+          }
+            var reservation = await _context.Reservation.FindAsync(id);
+
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return reservation;
+        }
+
+        // PUT: api/Reservation/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutReservation(int id, Reservation reservation)
+        {
+            if (id != reservation.id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(reservation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Reservation
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Reservation>> PostReservation(Reservation reservation)
+        {
+          if (_context.Reservation == null)
+          {
+              return Problem("Entity set 'GolfContext.Reservation'  is null.");
+          }
+            _context.Reservation.Add(reservation);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ReservationExists(reservation.id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetReservation", new { id = reservation.id }, reservation);
+        }
+
+        // DELETE: api/Reservation/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteReservation(int id)
+        {
+            if (_context.Reservation == null)
+            {
+                return NotFound();
+            }
+            var reservation = await _context.Reservation.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            _context.Reservation.Remove(reservation);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ReservationExists(int id)
+        {
+            return (_context.Reservation?.Any(e => e.id == id)).GetValueOrDefault();
+        }
+
+        [HttpPost("/api/reservaation/import")]
+        public async Task<IActionResult> ImportReservation(Reservation[] reservations)
+        {
+            if (_context.Reservation == null)
+            {
+                return Problem("Entity set 'GolfContext.Reservation'  is null.");
+            }
+
+            foreach (Reservation u in reservations)
+            {
+                _context.Reservation.Add(u);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ Golf/Program.cs
@@ -0,0 +1,62 @@
+
+using Golf.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Golf
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<GolfContext>(options => 
+            {
+                options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings:Golf").Value);
+                    
+            });
+            
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            // Add JWT Authentication Middleware - This code will intercept HTTP request and validate the JWT.
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+                opt => {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                        .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                }
+              );
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
